da2ea362d5b437ebd57dec66ded91d677c0a5033
TDB-star <tatiana.blache@gmail.com>
2024-08-14 14:13:39 +0200

    Added annotation @Singelton to apiService, databaseService & applicationComponent

--- app/src/main/java/com/example/androidcleanarchitecture/data/ApiService.kt
+++ app/src/main/java/com/example/androidcleanarchitecture/data/ApiService.kt
@@ -4,13 +4,15 @@ import android.content.Context
 import android.util.Log
 import com.example.androidcleanarchitecture.R
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ApiService @Inject constructor(
     private val context: Context
 ) {
 
     fun method() {
-        Log.d(LOG_TAG, "ApiService ${context.getString(R.string.app_name)}")
+        Log.d(LOG_TAG, "ApiService ${context.getString(R.string.app_name)}, $this")
     }
 
     companion object {
